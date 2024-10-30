3b3ced52c90796367c2a2aafc65988e74479ce4d
Mikhail Dyadchenko <mikhail_dyadchenko@mail.ru>
2024-10-30 03:44:47 +0300

    Initial Commit

---
+++ ONBOARD_MSD_App/ONBOARD_MSD_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ONBOARD_MSD_AppApp.swift
+//  ONBOARD_MSD_App
+//
+//  Created by Михаил Дядченко on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ONBOARD_MSD_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
